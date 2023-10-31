c6d0e99436ef51542c9beb6986515a9d1d568167
Zachary Linggon <Zachary.linggon@edu.sait.ca>
2023-10-30 21:19:35 -0600

    Step 3

---
+++ calculator.py
@@ -0,0 +1,17 @@
+#Calculator module for simple operations
+
+def add(first_number, second_number):
+    result = first_number + second_number
+    print(f"Result: {result}")
+
+def subtract(first_number, second_number):        
+    result = first_number - second_number
+    print(f"Result: {result}")
+
+def multiply(first_number, second_number):
+    result = first_number * second_number
+    print(f"Result: {result}")
+
+def divide(first_number, second_number):
+    result = first_number / second_number
+    print(f"Result: {result}")
